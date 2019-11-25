faeb82b3f93668130953eb4943e6c870243dc306
Tuan Cao <caotuan92.hus@gmail.com>
2019-11-25 17:36:08 +0700

    update example-project 2

--- scripts/db/restart.sh
+++ scripts/db/restart.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# !!! Please run this file in root project folder
+# This file is a shortcut for `docker-compose up -d`
+# USAGE:
+# $ script/start.sh [environment]
+# EG:
+# $ script/db/start.sh
+# $ script/db/start.sh mysql
+
+environment=$1
+
 cd ./develop-kit-databases
 docker-compose down
-docker-compose up -d
+
+case "$environment" in
+    mysql)
+        docker-compose -f docker-compose.mysql.yml up -d
+        ;;
+
+    pgsql)
+        docker-compose -f docker-compose.pgsql.yml up -d
+        ;;
+
+    mongo)
+        docker-compose -f docker-compose.mongo.yml up -d
+        ;;
+    *)
+        docker-compose -f docker-compose.yml up -d
+esac
